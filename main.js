f14fd849b14d7e2d6309c8b9f31c65540896c85b
tinuola <tinuola.awopetu@gmail.com>
2018-03-09 19:54:32 -0800

    displaying only unique rules, no duplicates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*jshint esversion: 6 */
 ////////// Initialize Firebase/////////////////
 var config = {
 	apiKey: "AIzaSyBDUg-JpP4C49m-nzuB_buQmAtJ_Kr3hoA",
@@ -11,72 +11,76 @@ var config = {
 firebase.initializeApp(config);
 ///////////////////////////////////////////////
 
+
+let ipsumDisplay = document.getElementById("ipsum-display");
+let userInputElem = document.getElementById("user-input");
+let submitElem = document.getElementById("submit-btn");
+
 //Firebase database reference object
 let rulesRef = firebase.database().ref("rules");
 
+//Store rules retrieved from Firebase
+let fbRulesArr;
+let randomRuleIndex;
 //Firebase event listener
-rulesRef.on("value", displayRules);
+rulesRef.on("value", getFbRules);
 
-//Store rules from Firebase
-let fbRulesArr = [];
-
-function displayRules (dataSnapshot){
-	
-	let rulesData = dataSnapshot.val();
-	rulesData.forEach(singleRule);
-	
-	//Display one random rule on start page
+function getFbRules (dataSnapshot){
+	fbRulesArr = dataSnapshot.val();
 	let starterRule = pickRandomRule();
-	let ipsumDisplay = document.getElementById("ipsum-display");
 	ipsumDisplay.innerHTML = starterRule;
+	console.log(fbRulesArr);
+}
 
-	//Generate a random rule
-	function pickRandomRule(){
-		let randomRule = Math.floor(Math.random()*fbRulesArr.length);
-		return fbRulesArr[randomRule];
-	}
+//Generate a random rule
+function pickRandomRule(){
+	/* Get a random rule between 1 and length of 
+	rules array (skips 0 index which is empty)*/
+	randomRuleIndex = Math.floor(Math.random() * (fbRulesArr.length-1) + 1);
+	return fbRulesArr[randomRuleIndex].description;
+}
 
-	//On submit button click, display user's request
-	let submitElem = document.getElementById("submit-btn");
-	submitElem.addEventListener("click", displayRules);
+//On submit button click, display user's request
+submitElem.addEventListener("click", displayRules);
 
-	//On pressing "enter" key after input, display user's request
-	let userInputElem = document.getElementById("user-input");
-	userInputElem.addEventListener("keypress", pressSubmit);
+//Display n # of rules
+function displayRules(){
+	let userInputVal = document.getElementById("user-input").value;
+	let requestedRulesArr = [];
+	let requestedRulesIndexArr = [];
+	let getRandomRule;
 
-	//Display n # of rules
-	function displayRules(){
-		let userInputVal = document.getElementById("user-input").value;
-		let displayUserRulesArr = [];
-		for (var i = 0; i < Number(userInputVal); i++){
-			displayUserRulesArr.push(pickRandomRule());
-		}
-		ipsumDisplay.innerHTML = removeCase(displayUserRulesArr).join(" ");
-	}
-
-	//Display rules on keypress "enter"
-	function pressSubmit(event){
-		if (event.keyCode === 13) {
-			displayRules();
+	while(requestedRulesArr.length < Number(userInputVal)){
+		getRandomRule = pickRandomRule();
+		if(!requestedRulesIndexArr.includes(randomRuleIndex)){
+			//randomRuleIndex is generated from pickRandomRule()
+			requestedRulesIndexArr.push(randomRuleIndex);
+			requestedRulesArr.push(getRandomRule);
 		}
 	}
+	console.log(requestedRulesArr);
+	ipsumDisplay.innerHTML = removeCase(requestedRulesArr).join(" ");
+}
 
-	//Remove case of rules
-	function removeCase(arr){
-		if(arr.length > 1){
-			var arrUncapped = [arr[0]];
-				for(var i = 1; i < arr.length; i++){
-					arrUncapped.push(arr[i].toLowerCase());
-				}
-			return arrUncapped;
-			} 
+//On pressing "enter" key after input, display user's request
+userInputElem.addEventListener("keypress", pressSubmit);
+
+//Display rules on keypress "enter"
+function pressSubmit(event){
+	if (event.keyCode === 13) {
+		displayRules();
 	}
+}
 
-} ////////End of Firebase Event Listener function
-
-//Retrieve each rule from Firebase
-function singleRule(ruleObj){
-	fbRulesArr.push(ruleObj.description);
+//Remove case of rules
+function removeCase(arr){
+	if(arr.length > 1){
+		var arrUncapped = [arr[0]];
+			for(var i = 1; i < arr.length; i++){
+				arrUncapped.push(arr[i].toLowerCase());
+			}
+		return arrUncapped;
+		} 
 }
 
 //Copy displayed rule(s) to clipboard
